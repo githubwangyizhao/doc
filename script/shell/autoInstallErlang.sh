13d21ffeecaae9394f15dbc69e51f120b574fba1
wangyizhao <913849845@qq.com>
2021-03-02 14:59:53 +0800

    modify shell script.

--- script/shell/autoInstallErlang.sh
+++ script/shell/autoInstallErlang.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+### =======================================
+# 安装前，需事先将安装包otp_src_20.3.tar.gz通过工具传到服务器/opt/bak/目录下。
+# 执行 ./autoInstallErlang.sh 
+### =======================================
 
+base_dir=/opt
 install_name=otp_src_20.3 			# 默认安装版本
-install_path=/opt/erl 				# 安装目录
-txt_begin="\033[32;49;1m [ "
-txt_end=" ] \033[39;49;0m"
+# install_path=/opt/erl 				# 安装目录
 
-print(){
-	echo -e $txt_begin$1$txt_end
+print() {
+	echo -e "\033[32;49;1m [ $1 ] \033[39;49;0m"
 }
 
 evn_func(){
@@ -26,29 +29,47 @@ install_func(){
 	yum -y install openssl-devel
 	yum -y install unixODBC-devel
 	
-	rm -rf $install_path
-	mkdir -p $install_path
-	cd $install_path
-	print "准备下载"
-	wget "http://www.erlang.org/download/$install_name.tar.gz"
-	stauts=$?
-	echo "是否下载成功：" `[ $stauts -eq 0 ] && echo "yes" || echo "no" `
-	if [ $stauts -eq 0 ]; then 
+	rm -rf $base_dir/$install_name
+	mkdir -p $base_dir/$install_name
+
+	# rm -rf $install_path
+	# mkdir -p $install_path
+	# cd $install_path
+	# print "准备下载"
+	# wget "http://www.erlang.org/download/$install_name.tar.gz"
+	# stauts=$?
+	# echo "是否下载成功：" `[ $stauts -eq 0 ] && echo "yes" || echo "no" `
+	# if [ $stauts -eq 0 ]; then 
+	#  	print "解压文件"
+	#  	sleep 3s 
+	# 	tar -zxvf $install_name.tar.gz
+	# 	rm -rf $install_path/$install_name.tar.gz
+		
+	# 	print "开始编译及安装" 
+	# 	cd $install_name
+	# 	./configure --prefix=/home/erlang --without-javac
+	# 	make
+	# 	make install
+	#  	return 2
+	# else
+	#  	echo "下载安装错误，文件名："$install_name
+	#  	return 3
+	# fi
+
+	if [ -f "$base_dir/bak/$install_name.tar.gz" ]; then 
 	 	print "解压文件"
-	 	sleep 3s 
-		tar -zxvf $install_name.tar.gz
-		rm -rf $install_path/$install_name.tar.gz
+		tar -zxvf $base_dir/bak/$install_name.tar.gz -C  $base_dir/
 		
 		print "开始编译及安装" 
-		cd $install_name
+		cd $base_dir/$install_name
 		./configure --prefix=/home/erlang --without-javac
 		make
 		make install
 	 	return 2
 	else
-	 	echo "下载安装错误，文件名："$install_name
+	 	echo "文件不存在："$base_dir/bak/$install_name.tar.gz
 	 	return 3
-	fi
+	 fi
 }
 
 main(){
@@ -82,7 +103,7 @@ main(){
 
 	if [ $? -eq 2 ]; then
 		# 添加软连
-		ln -s $install_path/$install_name/bin/erl /usr/local/bin/erl
+		ln -s $base_dir/$install_name/bin/erl /usr/local/bin/erl
 		print "安装成功！"
 	else
 		print "安装失败！"
